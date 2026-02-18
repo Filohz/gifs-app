2c447f3b5d6afa9cad5edf7884a1af07b05fa68b
jrocoh <jrocoh@gmail.com>
2026-02-18 13:30:50 -0300

    start gifs app

---
+++ src/GifsApp.tsx
@@ -0,0 +1,18 @@
+
+export const GifsApp = () => {
+  return (
+    <>
+        {/* header */}
+        <div className="content-center">
+            <h1>Buscador de Gifs</h1>
+            <p>Descubre y comparte el gif perfecto</p>
+        </div>
+
+        {/* search */}
+        <div className="search-container">
+            <input type="test" placeholder="Buscar gifs" />
+            <button>Buscar</button>
+        </div>
+    </>
+  )
+}
